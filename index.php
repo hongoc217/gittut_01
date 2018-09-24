a17e6da6c33a482979d51762afec30afacac5711
hongoc217 <hongoc217@gmail.com>
2018-09-24 13:31:43 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700|Acme" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="css/main.css">
+    </head>
+    <body>
+      <nav>
+        <ul>
+          <li class="active"><a href="/">About</a></li>
+          <li><a href="/">News</a></li>  
+          <li><a href="/">Contact</a></li>  
+        </ul>
+      </nav>
+      <h1>About us - On GitHub</h1>
+      <p>
+        Light Rail in Canberra offers an exciting opportunity to transform Canberra and deliver a truly integrated transport system, which will provide more options in how Canberrans move around, which in turn will enrich lifestyles and enhance growth.
+        <br>
+The first stage of Light Rail will connect the fast growing area of Gungahlin, through Dickson to the City.
+<br>
+There are 13 stops along the 12km track including a terminus at each end of the route. Construction has already started and services will begin for the community in 2018.
+<br>
+"Light Rail will bring local employment and a new industry in light rail operations, maintenance and hospitality for the next 20 years. This will drive growth, and growth will generate vibrancy, diversity of employment and long-term sustainability - just like it has in other cities around the world." Saori Peguicha, Project Manager, Pacific Partnerships (part of the consortium delivering stage one).
+<br>
+
+      </p>
+      
+    </body>
+  </html>
