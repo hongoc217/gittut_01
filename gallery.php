aee18b8e4293dfc22ff8a22122f99ad22bfddf5c
Hong Ngoc <hongoc217@gmail.com>
2018-09-24 14:23:44 +1000

    Adding gallery page and menu item

---
+++ gallery.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <title>Image Gallery</title>
+      <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700|Acme" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="main.css">
+    </head>
+    <body>
+      <nav>
+        <ul>
+          <li><a href="/">About</a></li>
+          <li><a href="/">News</a></li>
+          <li class="active"><a href="gallery.php">Gallery</a></li>
+          <li><a href="/">Contact</a></li>
+        </ul>
+      </nav>
+      <h1>Image Gallery - On GitHub</h1>
+      <p>
+        This is an image gallery. Photos coming soon.
+      </p>
+      
+    </body>
+  </html>
